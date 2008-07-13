259dcf3c4ea093534ac54b133d701e41faf7f3c1
Ryan <ry@tinyclouds.org>
2008-07-13 17:20:22 +0200

    just sketching out plans.
    decided that should use ragel more heavily since i find myself writing a
    highlevel parser which handles multiple requests. why not just write something
    which can parse pipelined requests?

--- ew.c
+++ ew.c
@@ -1,5 +1,4 @@
-
-
+#include <error.h>
 #include <ev.h>
 #include "ew.h"
 
@@ -8,12 +7,188 @@ static void set_nonblock
   )
 {
   int flags = fcntl(fd, F_GETFL, 0);
-  assert(0 <= fcntl(fd, F_SETFL, flags | O_NONBLOCK) && "Setting socket non-block failed!");
+  int r = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+  assert(0 <= r && "Setting socket non-block failed!");
+}
+
+/** 
+ * Must call this after ew_buf->save callback has been called.
+ * Allows you do to do async save operations (like writing to file).
+ * ew_buf->save, like all callbacks, MUST be non-blocking.  
+ */
+void ew_buf_save_finished
+  ( ew_buf *buf
+  )
+{
+  ;
 }
 
 
+/* Internal callback 
+ * called by request->read_watcher
+ */
+static void on_readable 
+  ( struct ev_loop *loop
+  , ev_io *watcher
+  , int revents
+  )
+{
+  ew_request *request = (ew_request*)(watcher->data);
 
-/* Internal callback */
+  if(request->has_read_head)
+    if(request->headers.chunked_encoding) 
+      goto read_body_chunked;
+    else
+      goto read_body_normal;
+  else
+    goto read_header;
+
+/* read the header into request->header_buf. overflow is not tolerated.
+ *
+ */
+read_header:
+  if(EW_MAX_HEADER_SIZE <= request->read) {
+    /* header buffer overflow. increase EW_MAX_HEADER_SIZE? */
+    goto error;
+  }
+  
+  ssize_t read = recv( request->connection->fd
+                     , request->header_buf + request->read
+                     , EW_MAX_HEADER_SIZE - request->read
+                     , 0
+                     );
+
+  if(read < 0) goto error;
+  if(read == 0) goto error; /* XXX is this the right action to take for read==0 ? */
+  request->read += read;
+
+  ev_timer_again(loop, watcher);
+
+  request_headers_parse( &request->headers
+                       , request->header_buf
+                       , request->read
+                       );
+
+  if(request_headers_has_error(&request->headers)) 
+    goto error;
+
+  if(!request_headers_is_finished(&request->headers))
+    return;
+
+  /* otherwise we're finished */
+
+  request->has_read_head = TRUE;
+
+  assert(request->read >= request->headers.nread);
+  unsigned int left_over = request->read - request->headers.nread;
+  
+  if(left_over > 0) {
+    if(request->headers.chunked)
+      goto read_body_chunked;
+    else
+      goto read_body_normal;
+  }
+  if(content_length == 0 && left_over > 0)
+  if(request->headers.method == EW_GET || request->headers.method == EW_HEAD) {
+    ev_io_stop(&request->read_watcher);
+    /* start a new requestuest with left_over? */
+    assert(left_over == 0 && "left_over == 0 for get/head. ");
+     
+  } else {
+    unsigned int left_over = request->read - request->headers.len;
+    ew_buf *buf = request->get_buf(request, left_over);
+    memcpy(buf->buf, request->header_buf + request->headers.len, left_over);
+    request->body = buf;
+  }
+
+recv_unknown_amount:
+  ew_recv_buf *recv_buf = NULL;
+
+read_body_chunked:
+  assert(0 && "Not Implemented");
+  return;
+
+read_body_normal:
+  unsigned int needed;
+
+  if(request->headers->content_length > 0) {
+    needed = request->headers->content_length - request->read;
+    buf = request->get_buf(request, needed);
+  } else {
+    /* need a buf but unknown size */
+    buf = request->get_buf(request, 0);
+  }
+
+  if(buf == NULL) goto error;
+
+  assert(buf->max_len > buf->len);
+
+  ssize_t read = recv( request->connection->fd
+                     , buf->buf + buf->len
+                     , buf->max_len - buf->len
+                     , 0
+                     );
+  if(read < 0) goto error;
+  if(read == 0) goto error; /* XXX is this the right action to take for read==0 ? */
+  buf->len += read;
+  request->read += read;
+
+  ev_timer_again( request->connection->server->loop
+                , &request->connection->timeout_watcher
+                );
+
+  if(request->headers.content_length > 0) {
+    if(request->read - request->header_buf.len == request->headers->content_length)
+      finished!
+  } else {
+  }
+  return;
+
+error:
+}
+
+/* Internal callback 
+ * Called by when a connection sends a new request.
+ * Seperated from on_connection() because Keep-Alive connections can have
+ * multiple requests
+ * this is callback might be called multiple times per connection.
+ */
+static void on_request 
+  ( struct ev_loop *loop
+  , ev_io *watcher
+  , int revents
+  )
+{
+  ew_connection *connection = (ew_connection*)(watcher->data);
+  
+  assert(connection->open);
+  assert(connection->server->listening);
+  assert(connection->server->loop == loop);
+  assert(&connection->read_watcher == watcher);
+
+  ev_io_stop(loop, watcher);
+
+  ew_request *request = NULL;
+  if(connection->new_request)
+    request = connection->new_request(connection);
+  if(request == NULL) {
+    return;
+  }
+
+  request->connection = connection;
+  ev_io_set(&request->read_watcher, connection->fd, EV_READ | EV_ERROR);
+  /* XXX: more reason for connections to have special error watchers */
+  ev_io_start(loop, &request->read_watcher);
+  /* XXX: the callback for the read_watcher 
+   * should be called immediately? or should i call it manually?
+   */
+
+  ev_timer_again(loop, &connection->timeout_watcher);
+}
+
+/* Internal callback 
+ * Called by server->connection_watcher.
+ */
 static void on_connection
   ( struct ev_loop *loop
   , ev_io *watcher
@@ -22,16 +197,15 @@ static void on_connection
 {
   ew_server *server = (ew_server*)(watcher->data);
 
-  assert(server->open);
+  assert(server->listening);
   assert(server->loop == loop);
-  assert(&server->req_watcher == watcher);
+  assert(&server->connection_watcher == watcher);
   
   if(EV_ERROR & revents) {
-    g_message("on_connection() got error event, closing server.");
+    error(0, 0, "on_connection() got error event, closing server.\n");
     ew_server_unlisten(server);
     return;
   }
-
   
   struct sockaddr_in addr; // connector's address information
   socklen_t addr_len = sizeof(their_addr); 
@@ -44,45 +218,65 @@ static void on_connection
     return;
   }
 
-  
-  ew_connection *connection = server->connection_handler(server, addr);
-  
+  ew_connection *connection = NULL;
+  if(server->new_connection)
+     connection = server->new_connection(server, addr);
   if(connection == NULL) {
-    close(fd);    
+    close(fd);
     return;
   } 
-
+  
   set_nonblock(fd);
   connection->fd = fd;
   connection->open = TRUE;
   connection->server = server;
-  
   memcpy(&connection->sockaddr, &addr, addr_len);
   
   if(server->port[0] != '\0')
     connection->ip = inet_ntoa(addr.sin_addr);  
 
-  connection->write_watcher.data = connection;
-  ev_init (&connection->write_watcher, on_writable);
-  ev_io_set (&connection->write_watcher, connection->fd, EV_WRITE);
-  /* Note: not starting the write watcher until there is data to 
-   * be written
-   */
+  /* Note: not starting the write watcher until there is data to be written */
+  ev_io_set(&connection->write_watcher,   connection->fd, EV_WRITE);
+  ev_io_set(&connection->request_watcher, connection->fd, EV_READ | EV_ERROR);
+  /* XXX: seperate error watcher? */
 
-  connection->req_watcher.data = connection;
-  ev_init(&connection->req_watcher, on_reqable);
-  ev_io_set(&connection->req_watcher, connection->fd, EV_READ | EV_ERROR);
-  ev_io_start(server->loop, &connection->req_watcher);
-  
-  connection->timeout_watcher.data = connection;  
-  ev_timer_init(&connection->timeout_watcher, on_timeout, connection->timeout, 0);
-  ev_timer_start(connection->server->loop, &connection->timeout_watcher);
+  ev_io_start(loop, &connection->request_watcher);
+  ev_timer_start(loop, &connection->timeout_watcher);
+}
+
+
+static ew_request* request_new
+  ( ew_connection *connection
+  , const char *intial_data
+  , size_t initial_data_len 
+  )
+{
+  ew_request *request = connection->request_handler(connection);
+
+  if(request == NULL) {
+    return NULL;
+  }
+
+  request->connection = connection; 
+
+  assert(initial_data_len < EW_MAX_HEADER_SIZE);
+
+  memcpy(request->header_buf, initial_data, initial_data_len);
+  request->read = intial_data_len;
+
+  request->read_watcher.data = request;
+  ev_init(&request->read_watcher, read_request);
+  ev_io_set(&request->read_watcher, connection->fd, EV_READ | EV_ERROR);
+  ev_io_start(connection->loop, &request->read_watcher);
+  /* Note, not starting the read_watcher until there is something to be
+   * a request is made
+   */
 }
 
 /**
  * begin the server listening on a file descriptor
- * @param  server pointer to ew_server
- * @param  sfd    the descriptor to listen on
+ * Thie DOES NOT start the event loop. That is your job.
+ * Start the event loop after the server is listening.
  */
 int ew_server_listen_on_fd
   ( ew_server *server
@@ -97,11 +291,9 @@ int ew_server_listen_on_fd
   set_nonblock(sfd); /* XXX superfluous? */
   
   server->fd = sfd;
-  assert(server->open == FALSE);
-  server->open = TRUE;
+  assert(server->listening == FALSE);
+  server->listening = TRUE;
   
-  server->connection_watcher.data = server;
-  ev_init (&server->connection_watcher, on_connection);
   ev_io_set (&server->connection_watcher, server->fd, EV_READ | EV_ERROR);
   ev_io_start (server->loop, &server->connection_watcher);
   
@@ -109,6 +301,11 @@ int ew_server_listen_on_fd
 }
 
 
+/**
+ * begin the server listening on a localhost TCP port
+ * Thie DOES NOT start the event loop. That is your job.
+ * Start the event loop after the server is listening.
+ */
 int ew_server_listen_on_port
   ( ew_server *server
   , const int port
@@ -155,20 +352,105 @@ error:
   return -1;
 }
 
+/**
+ * Stops a server from listening. Will not accept new connections.
+ * TODO: Drops all connections?
+ */
+void ew_server_unlisten
+  ( ew_server *server
+  )
+{
+  if(server->listening) {
+    ev_io_stop(server->loop, &server->connection_watcher);
+    close(server->fd);
+    server->port = "\0";
+    server->listening = FALSE;
+  }
+}
 
+/**
+ * Initialize an ew_server structure.
+ * After calling ew_server_init set the callback server->new_connection 
+ * and, optionally, callback data server->data 
+ *
+ * @params server the server to initialize
+ * @params loop a libev loop
+ */
 void ew_server_init
   ( ew_server *server
   , struct ev_loop *loop
-  , ew_connection_handler handler
-  , void *data
   )
 {
-  server->connection_handler = handler;
-  server->data = data;
   server->loop = loop;
-  server->open = FALSE;
-
+  server->listening = FALSE;
   server->port = "\0";
-
   server->fd = -1;
+  server->connection_watcher.data = server;
+  ev_init (&server->connection_watcher, on_connection);
+
+  server->new_connection = NULL;
+  server->data = NULL;
+}
+
+/**
+ * Initialize an ew_connection structure.
+ * After calling ew_connection_init set the callback 
+ * connection->new_request 
+ * and, optionally, callback data connection->data 
+ * 
+ * This should be called immediately after allocating space for
+ * a new ew_connection structure. Most likely, this will only 
+ * be called within the ew_server->new_connection callback which
+ * you supply. 
+ *
+ * @params connection the connection to initialize
+ * @params timeout    the timeout in seconds
+ */
+void ew_connection_init
+  ( ew_connection *connection
+  , float timeout
+  )
+{
+  connection->fd = -1;
+  connection->server = NULL;
+  connection->ip = "\0";
+  connection->open = FALSE;
+  connection->timeout = timeout;
+  
+  connection->write_watcher.data = connection;
+  ev_init (&connection->write_watcher, on_writable);
+
+  connection->request_watcher.data = connection;
+  ev_init(&connection->request_watcher, on_request);
+
+  connection->timeout_watcher.data = connection;  
+  ev_timer_init(&connection->timeout_watcher, on_timeout, timeout, 0);
+
+
+  connection->new_request = NULL;
+  connection->free = NULL;
+  connection->data = NULL;
+}
+
+/** 
+ * Initialze ew_request structure. Call this from your connection->new_request
+ * callback
+ */
+void ew_request_init
+  ( ew_request *request
+  ) 
+{
+  request->connection = NULL;
+  request_headers_init(&request->headers);
+  request->header_buf[0] = '\0';
+  request->read = 0;
+  request->read_body_normal = 0;
+  request->has_read_head = FALSE;
+
+  request->read_watcher.data = request;
+  ev_init(&request->read_watcher, on_readable);
+
+  request->new_buf = NULL;
+  request->free = NULL;
+  request->data = NULL;
 }
--- ew.h
+++ ew.h
@@ -1,46 +1,39 @@
-
 #define EW_MAX_CLIENTS 1024
 #define EW_MAX_HEADER_SIZE 1024*8
 
-typedef struct ew_buf      ew_buf;
-typedef struct ew_server     ew_server;
+typedef struct ew_buf ew_buf;
+typedef struct ew_server ew_server;
 typedef struct ew_connection ew_connection;
-typedef struct ew_req    ew_req;
+typedef struct ew_request ew_request;
+typedef struct ew_res ew_res;
 
 
-typedef void (*ew_buf_handler)(ew_buf *buf);
 struct ew_buf {
   unsigned char   *buf;
-  unsigned int     len;
-  unsigned int     max_len;
+  size_t len;
+  size_t max_len;
 
-
-  ew_buf_handler  free; 
-  ew_buf_handler  save;  // return EW_FINISHED or EW_NOT_FINISHED
-  void            *data; 
-
-  // must be called when save is finished if EW_NOT_FINSIHED
-  // was returned 
-  ew_buf_handler  save_finished;  
-
+  void (*free) (ew_buf*);
+  void (*save) (ew_buf*);
+  void *data; 
 };
 
-typedef ew_connection* (*ew_connection_handler)
-  ( ew_server *server
-  , struct sockaddr_in *sockaddr
-  ); 
+void ew_buf_save_finished
+  ( ew_buf *buf
+  );
 
 struct ew_server {
-  int fd;
-  struct sockaddr_in        sockaddr;
-  socklen_t                 socklen;    /* size of sockaddr */ 
-  char                      port[8];    /* port as a string */
+  int fd;                      /* ro */
+  struct sockaddr_in sockaddr; /* ro */
+  socklen_t socklen;           /* ro */
+  char port[6];                /* ro */
+  struct ev_loop *loop;        /* ro */
+  ev_io connection_watcher;    /* private */
+  unsigned listening:1;        /* ro */
 
-  struct ev_loop           *loop;
-
-  ev_io                     connection_watcher;
-  ew_connection_handler     connection_handler;
-
+  /* public */
+  ew_connection* (*new_connection) (ew_server*);
+  void (*free) (ew_server*);
   void *data;
 };
 
@@ -48,8 +41,6 @@ struct ew_server {
 void ew_server_init
   ( ew_server *server
   , struct ev_loop *loop
-  , ew_connection_handler handler
-  , void *data
   );
 
 int ew_server_listen_on_port
@@ -62,48 +53,51 @@ int ew_server_listen_on_fd
   , const int sfd 
   );
 
+void ew_server_unlisten
+  ( ew_server *server
+  );
 
-/********** CONNECTION ******/
-
-typedef ew_req* (*ew_req_handler) (ew_connection *connection);
+/********** CONNECTION **********/
 
 struct ew_connection {
-  int                   fd;
-  struct sockaddr_in    sockaddr;
-  socklen_t             socklen;    /* size of sockaddr */ 
-  ew_server            *server
+  int fd;                      /* ro */
+  struct sockaddr_in sockaddr; /* ro */
+  socklen_t socklen;           /* ro */ 
+  ew_server *server            /* ro */
+  float timeout;               /* ro */
+  char ip[40];                 /* ro */
+  unsigned open:1;             /* ro */
+  ev_io request_watcher;       /* private */
+  ev_io write_watcher;         /* private */
+  ev_timer timeout_watcher;    /* private */
 
-  float timeout;
-  char *ip;
-  unsigned open:1; 
-
-  ew_req_handler   req_handler;
-
+  /* public */
+  ew_request* (*new_request) (ew_connection*);
+  void (*free) (ew_connection*);
   void *data;
-}; 
+};
 
 /********** REQUEST *********/
 
-typedef ew_buf* (*ew_buf_get) (ew_req *req, size_t needed);
-typedef void    (*ew_buf_save) (ew_buf *buf);
 
-struct ew_req {
-  ew_connection *connection; 
-  ew_req_headers headers;
+struct ew_request {
+  ew_connection *connection;           /* ro */
+  request_headers headers;             /* ro */
+  char header_buf[EW_MAX_HEADER_SIZE]; /* ro */
+  size_t read;                         /* ro */
+  size_t read_body_normal;             /* private */
+  unsigned has_read_head:1;            /* ro */
+  ev_io read_watcher;                  /* private */
 
-  char header_buf[EW_MAX_HEADER_SIZE];
-
-  ew_buf *body;
-
-  unsigned int read;
-
-  unsigned has_read_head:1;
+  /* public */
+  ew_buf* (*new_buf)(ew_request*, size_t needed);
+  void (*free) (ew_request*);
+  void *data;
 };
 
 
 /********** RESPONSE ********/
-typedef struct ew_res ew_res;
 struct ew_res {
-  ew_req          *req; 
+  ew_request          *request; 
   ew_connection       *connection; 
 };
